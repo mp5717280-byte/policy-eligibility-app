60e9a29341d9d6fcb8453005a5c66c4cee6a5c11
mp5717280-byte <mp5717280@gmail.com>
2025-09-14 03:55:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import re
+from datetime import date
 
 
 st.set_page_config(page_title="LIC Plan Eligibility (Step 1 & 2)", layout="centered")
